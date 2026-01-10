5dc6ecb51b0805d3f60989cf45921af313523c5d
Renato Nunes Junior <renatonj0489@gmail.com>
2026-01-10 17:59:37 -0300

    Fix: Unificando rotas no App e adicionando vercel rewrite

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Agendamento from "./pages/Agendamento";
-import Financas from "./pages/Financas";
-import Cadastro from "./pages/Cadastro";
+import App from "./App"; // Importa o seu App que já tem as rotas e o login
 import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/agendamento" element={<Agendamento />} />
-        <Route path="/financas" element={<Financas />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
